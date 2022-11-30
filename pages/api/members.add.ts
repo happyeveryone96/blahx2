87838ad5e4a4c369f5c0551e0506a9f510256956
happyeveryone96 <happyeveryone@naver.com>
2022-11-30 22:31:43 +0900

    feat: 사용자 추가 API 구현

---
+++ pages/api/members.add.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import FirebaseAdmin from '@/models/firebase_admin';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { uid, email, displayName, photoURL } = req.body;
+  if (uid === undefined || uid === null) {
+    return res.status(400).json({ result: false, message: 'uid가 누락되었습니다.' });
+  }
+  if (email === undefined || email === null) {
+    return res.status(400).json({ result: false, message: 'email이 누락되었습니다.' });
+  }
+
+  try {
+    const screenName = (email as string).replace('@gmail.com', '');
+
+    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
+      const meberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
+      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
+      const memberDoc = await transaction.get(meberRef);
+      if (memberDoc.exists) {
+        return false;
+      }
+      const addData = {
+        uid,
+        email,
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? '',
+      };
+      await transaction.set(meberRef, addData);
+      await transaction.set(screenNameRef, addData);
+      return true;
+    });
+    if (addResult === false) {
+      return res.status(201).json({ result: true, id: uid });
+    }
+    return res.status(200).json({ result: true, id: uid });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ result: false });
+  }
+}
